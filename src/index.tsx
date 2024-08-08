b35685ec857c5883e92a59974e38c861c6d56072
Benjy <ben@pngs.cc>
2024-08-08 22:22:26 +0800

    chore: bruno

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,90 @@
 import '@logseq/libs'
+
 import { handlePopup } from './handle-popup'
 import { settings } from './settings'
 
-const main = async () => {
-  console.log('<insert-plugin-name> loaded')
+export interface ZoteroItem {
+  key: string
+  version: number
+  library: {
+    type: string
+    id: number
+    name: string
+    links: {
+      self: {
+        href: string
+        type: string
+      }
+      alternate: {
+        href: string
+        type: string
+      }
+    }
+  }
+  links: {
+    self: {
+      href: string
+      type: string
+    }
+    alternate: {
+      href: string
+      type: string
+    }
+    attachment?: {
+      href: string
+      type: string
+      attachmentType: string
+      attachmentSize: number
+    }
+    up?: {
+      href: string
+      type: string
+    }
+    enclosure?: {
+      href: string
+      type: string
+      title: string
+      length: number
+    }
+  }
+  meta: {
+    creatorSummary: string
+    parsedDate: string
+    numChildren: number
+  }
+  data: {
+    key: string
+    version: number
+    itemType: string
+    title: string
+    date: string
+    language: string
+    shortTitle?: string
+    libraryCatalog: string
+    url: string
+    accessDate: string
+    volume?: string
+    pages?: string
+    publicationTitle: string
+    DOI: string
+    issue?: string
+    journalAbbreviation?: string
+    ISSN?: string
+    creators: {
+      firstName: string
+      lastName: string
+      creatorType: string
+    }[]
+    tags: any[]
+    collections: any[]
+    relations: any
+    dateAdded: string
+    dateModified: string
+  }
+}
 
+const main = async () => {
+  console.log('logseq-zoterolocal-plugin loaded')
   // Used to handle any popups
   handlePopup()
 }
