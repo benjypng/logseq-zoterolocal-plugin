2dba3f3fb1514a75e6a93977615e236f7f233271
Benjy <ben@pngs.cc>
2024-08-09 19:59:19 +0800

    chore: working on inserting zot item

--- src/services/insert-zot-into-graph.ts
+++ src/services/insert-zot-into-graph.ts
@@ -2,6 +2,47 @@ import { BlockEntity } from '@logseq/libs/dist/LSPlugin'
 
 import { ZotData } from '../features/main/interfaces'
 
+const replaceTemplateWithValues = (template: string, data: ZotData) => {
+  const keys = Object.keys(data).filter(
+    (key) => key !== 'authors' && key !== 'attachment' && key !== 'relations',
+  )
+  let result = template
+
+  for (const key of keys) {
+    const placeholder = new RegExp(`<% ${key} %>`, 'g')
+    // @ts-expect-error maybe can type keys later
+    const value = data[key]
+
+    if (
+      value === undefined ||
+      value === null ||
+      value === '' ||
+      (Array.isArray(value) && value.length === 0)
+    ) {
+      // Remove the entire line if the value is empty
+      result = result.replace(new RegExp(`^.*<% ${key} %>.*$\n?`, 'gm'), '')
+    } else if (typeof value === 'object' && Array.isArray(value)) {
+      // Skip object values (except arrays)
+      continue
+    } else {
+      result = result.replace(placeholder, value)
+    }
+  }
+
+  result = result.replace(
+    /^.*<% (firstName|lastName|creatorType) %>.*$\n?/gm,
+    '',
+  )
+
+  result = result.replace(/^.*<% attachment %>.*$\n?/gm, '')
+
+  result = result.replace(/^.*<%.*%>.*$\n?/gm, '')
+
+  result = result.replace(/\n{2,}/g, '\n').trim()
+
+  return result
+}
+
 export const insertZotIntoGraph = async (zotItem: ZotData) => {
   const templateName = logseq.settings!.zotTemplate as string
   if (!templateName && templateName.length === 0) {
@@ -21,26 +62,36 @@ export const insertZotIntoGraph = async (zotItem: ZotData) => {
     return
   }
 
-  const { children: template } = (await logseq.Editor.getBlock(
-    templateRoot.uuid,
-    {
-      includeChildren: true,
-    },
-  )) as BlockEntity
-  if (template!.length === 0) {
+  const template = (await logseq.Editor.getBlock(templateRoot.uuid, {
+    includeChildren: true,
+  }))!.children as BlockEntity[]
+
+  if (template.length === 0) {
     logseq.UI.showMsg(`Please ensure a template is created`, 'error')
     return
   }
+
+  const templateStr = replaceTemplateWithValues(template[0].content, zotItem)
 
   const pageName = logseq.settings!.useCiteKeyForTitle
     ? zotItem.citeKey
     : zotItem.title
 
+  const page = await logseq.Editor.getPage(pageName)
+  if (!page) {
+    await logseq.Editor.createPage(
+      pageName,
+      {},
+      {
+        redirect: true,
+        createFirstBlock: false,
+        journal: false,
+      },
+    )
+    await logseq.Editor.appendBlockInPage(pageName, templateStr)
+  }
+
   // Create page according to template
 
-  // Go to page
-  //logseq.App.pushState('page', {
-  //  name: pageName,
-  //})
   //logseq.hideMainUI()
 }
