480cd01f308063a8b3d2325b6ddca0dc24a51821
Benjy <ben@pngs.cc>
2024-08-14 00:49:09 +0800

    fix: remove refetch

--- src/hooks/use-items.ts
+++ src/hooks/use-items.ts
@@ -6,6 +6,7 @@ import { getZotItems } from '../services/get-zot-items'
 export const useZotItems = () => {
   return useQuery<ZotData[], Error>('zotItems', getZotItems, {
     retry: false,
+    refetchOnWindowFocus: false,
     onError: (error: Error) => {
       console.error('Error fetching Zotero items:', error)
     },
